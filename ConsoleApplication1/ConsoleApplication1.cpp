c9564124f519e7809572b18ec0adf51828d8d58e
Damon-Liu <43329333+Damon-Liu@users.noreply.github.com>
2018-10-12 20:44:02 +0800

    Update ConsoleApplication1.cpp

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -21,50 +21,54 @@ char speChar(char hex) {
 
 int main(int arg, char* argc[])
 {
-	//Defining format	
-	int* addr = (int*)0x00000000;
-	printf("\t\t00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F \n");
-	printf("\t\t===============================================");
-	printf("\n%p\t", addr);
-	//Opening file
-	FILE* pFile;
-	int i = 0, j = 0;
-	char* filename = (char*)argc[1];
-	int file = fopen_s(&pFile, filename, "rb");
-	//Calculation of file size
-	fseek(pFile, 0, SEEK_END);
-	int filesize = ftell(pFile);
-	//Point the file pointer back to file original
-	fseek(pFile, 0, SEEK_SET);
-	//Create dynamic buffer in heap zone
-	unsigned char* buf = (unsigned char*)malloc(sizeof(char)*filesize);
-	memset(buf, 0, filesize);
-	if (file == 0) {
-		int read = fread_s(buf, sizeof(char)*filesize, filesize, 1, pFile);
-		while (i <filesize) {//filesize
-			if (j<16) {
-				printf("%02x ", buf[i]);
-				j++;
-				i++;
-			}
-			else {
-				j = i - 16;
-				printf(" \t");
-				while (j<i) {
-					printf("%c ", speChar(buf[j]));
+	if(!argc[1]){
+	   return 0;
+	}else{
+		//Defining format	
+		int* addr = (int*)0x00000000;
+		printf("\t\t00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F \n");
+		printf("\t\t===============================================");
+		printf("\n%p\t", addr);
+		//Opening file
+		FILE* pFile;
+		int i = 0, j = 0;
+		char* filename = (char*)argc[1];
+		int file = fopen_s(&pFile, filename, "rb");
+		//Calculation of file size
+		fseek(pFile, 0, SEEK_END);
+		int filesize = ftell(pFile);
+		//Point the file pointer back to file original
+		fseek(pFile, 0, SEEK_SET);
+		//Create dynamic buffer in heap zone
+		unsigned char* buf = (unsigned char*)malloc(sizeof(char)*filesize);
+		memset(buf, 0, filesize);
+		if (file == 0) {
+			int read = fread_s(buf, sizeof(char)*filesize, filesize, 1, pFile);
+			while (i <filesize) {//filesize
+				if (j<16) {
+					printf("%02x ", buf[i]);
 					j++;
+					i++;
 				}
-				j = 0;
-				printf("\n%p\t",addr+=4);
+				else {
+					j = i - 16;
+					printf(" \t");
+					while (j<i) {
+						printf("%c ", speChar(buf[j]));
+						j++;
+					}
+					j = 0;
+					printf("\n%p\t",addr+=4);
+				}
+
 			}
-			
 		}
-	}
-	else {
+		else {
+			fclose(pFile);
+			return 0;
+		}
 		fclose(pFile);
-		return 0;
+		free(buf);
 	}
-	fclose(pFile);
-	free(buf);
 }
 
